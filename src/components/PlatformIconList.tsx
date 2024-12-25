b5e381d6af0fb971e6d288f9ff0327782be698f4
Medo Haleem <medo.a.haleem@gmail.com>
2024-12-25 10:18:46 +0200

    add metacritic score

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Platform} from "../hooks/useGames";
-import {HStack, Icon, Text} from "@chakra-ui/react";
+import {HStack, Icon} from "@chakra-ui/react";
 import {
     BsGlobe,
     FaAndroid,
