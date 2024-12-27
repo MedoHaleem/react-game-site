b0f19a55fba77c120e03f30f4c0e26a74f9e41e4
Medo Haleem <medo.a.haleem@gmail.com>
2024-12-27 09:41:09 +0200

    Display Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import useGenres from "../hooks/useGenres";
+import {HStack, Image, List, ListItem, Text} from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
     const {data} = useGenres()
     return (
-        <ul>
-            {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
-        </ul>
+        <List>
+            {data.map(genre => <ListItem paddingY='5px' key={genre.id}><HStack>
+                <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
+                <Text fontSize='lg'>{genre.name}</Text>
+            </HStack></ListItem>)}
+        </List>
     );
 };
 
