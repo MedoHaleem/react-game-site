87ccb2a352bbd550d5c14d2a42ec7b41a66a0247
Medo Haleem <medo.a.haleem@gmail.com>
2024-12-27 09:33:43 +0200

    Add Generic Hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import './App.css'
 import {Grid, GridItem, Show} from '@chakra-ui/react'
 import Navbar from "./components/Navbar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
     return <Grid templateAreas={{base: `"nav" "main"`, lg: `"nav nav" "aside main"`}}>
         <GridItem area='nav'><Navbar/></GridItem>
         <Show above='lg'>
-            <GridItem area='aside'>Aside</GridItem>
+            <GridItem area='aside'><GenreList /></GridItem>
         </Show>
         <GridItem area='main'><GameGrid /></GridItem>
     </Grid>
